e3c8d6a500bdde2e677c98b2bb8ae7548b53df57
Kok Wai <teng.kokwai93@gmail.com>
2021-09-06 00:01:51 +0800

    init coding handbook, infinite games

--- src/styles/global.js
+++ src/styles/global.js
@@ -93,6 +93,26 @@ const globalStyles = css`
     margin: 40px auto;
     align-content: center;
   }
+
+  .video-container {
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+  }
+
+  .video-container::after {
+    padding-top: 56.25%;
+    display: block;
+    content: "";
+  }
+
+  .video-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 `
 
 export default globalStyles
