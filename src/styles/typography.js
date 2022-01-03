dc48787a59f1dec5f1881b81379393dd8af007cc
Kok Wai <teng.kokwai93@gmail.com>
2022-01-03 23:38:56 +0800

    aesthetics

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -18,29 +18,28 @@ const typeStyles = css`
 
   h2 {
     margin-bottom: 2rem;
-    font-size: 1.9em;
-    line-height: 1.1;
+    font-size: 2.4rem;
   }
 
   h3 {
     line-height: 1.2;
-    font-size: 1.5em;
+    font-size: 2rem;
   }
 
   h4 {
     line-height: 1.1;
-    font-size: 1.3em;
+    font-size: 1.6em;
   }
 
   h5 {
     margin: 1.2rem 0;
     font-weight: 500;
     line-height: 20px;
-    font-size: 0.95em;
+    font-size: 1.2rem;
   }
 
   h6 {
-    font-size: 0.9em;
+    font-size: 0.8rem;
     font-weight: 500;
     margin: 0;
   }
@@ -59,9 +58,25 @@ const typeStyles = css`
     background-color: var(--color-backgroundOffset, #ffffff);
     border-left: 5px solid var(--color-primaryOffset, #3672f8);
     margin: 20px 0;
+    font-family: var(--fontFamily-sans);
   }
 
-  blockquote p {
+  blockquote p:first-child {
+    margin: 1rem 1rem 0.2rem 1rem;
+    padding: 1rem 1rem 0.2rem 1rem;
+  }
+
+  blockquote p:not(:first-child):not(:last-child) {
+    margin: 0.2rem 1rem 0.2rem 1rem;
+    padding: 0.2rem 1rem 0.2rem 1rem;
+  }
+
+  blockquote p:last-child {
+    margin: 0.2rem 1rem 1rem 1rem;
+    padding: 0.2rem 1rem 1rem 1rem;
+  }
+
+  blockquote p:only-child {
     margin: 1rem;
     padding: 1rem;
   }
