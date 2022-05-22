7fa3cafc4579e67e3c9f85c775eb945299c6f68a
Kok Wai <teng.kokwai93@gmail.com>
2022-05-22 17:03:58 +0800

    update spark ar articles

--- src/components/mdx/ImageStyles.js
+++ src/components/mdx/ImageStyles.js
@@ -7,20 +7,40 @@ export const Img = styled.img`
 `
 
 export const BasicImage = (props) => {
-  return (
-    <div align="center">
-      <img
-        css={css({
-          width: props.width || "100%",
-          maxWidth: props.maxwidth || "1200px",
-          display: "block",
-          margin: props.margin || "0",
-          padding: props.padding || "1rem 0",
-          borderRadius: "4px",
-        })}
-        src={props.src}
-        alt={props.alt}
-      />
-    </div>
-  )
+  if (props.link)
+    return (
+      <div align="center">
+        <a href={props.link} target="_blank">
+          <img
+            css={css({
+              width: props.width || "100%",
+              maxWidth: props.maxwidth || "1200px",
+              display: "block",
+              margin: props.margin || "0",
+              padding: props.padding || "1rem 0",
+              borderRadius: "4px",
+            })}
+            src={props.src}
+            alt={props.alt}
+          />
+        </a>
+      </div>
+    )
+  else
+    return (
+      <div align="center">
+        <img
+          css={css({
+            width: props.width || "100%",
+            maxWidth: props.maxwidth || "1200px",
+            display: "block",
+            margin: props.margin || "0",
+            padding: props.padding || "1rem 0",
+            borderRadius: "4px",
+          })}
+          src={props.src}
+          alt={props.alt}
+        />
+      </div>
+    )
 }
